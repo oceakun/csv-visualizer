12dec7fe4b555c7f9756a41350e7658914c44530
Sagar Deep <sagardeep1811@hotmail.com>
2023-03-31 00:47:03 +0530

    add a component that describes the app-flow

---
+++ src/components/instructionsContainer/InstructionsContainer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function InstructionsContainer() {
+  return (
+    <div className="instructionsContainer">
+      <h1>Hi!</h1>
+      <ul>
+        <li>press 'Upload', a modal window will appear </li>
+        <li>
+          browse through your computer to choose a file, then press 'submit'
+        </li>
+        <li>press 'Visualize'</li>
+      </ul>
+    </div>
+  );
+}
