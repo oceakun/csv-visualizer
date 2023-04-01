47b47edb7bb0e6c483e8305628ebb2e651d33f66
Sagar Deep <sagardeep1811@hotmail.com>
2023-04-01 22:10:32 +0530

    add functions to populate and depopulate the UserStoredFiles container

--- src/components/userSpecificAssets/UserStoredFiles.jsx
+++ src/components/userSpecificAssets/UserStoredFiles.jsx
@@ -1,87 +1,67 @@
 import React, { useState, useEffect } from "react";
 import { useStore } from "@nanostores/react";
 import { userSpecificAssetsDisplayStatus } from "../../store/ComponentsDisplayPropertyStore";
-import { currentUserUid } from "../../store/UserStore";
+import {
+  currentUserUid,
+  filesfetchedFromFirestore,
+} from "../../store/UserStore";
 import { newFileSaved } from "../../store/SavedFileStore";
 import { db } from "../../firebase";
-import {
-  collection,
-  getDocs,
-  addDoc,
-  updateDoc,
-  deleteDoc,
-  doc,
-} from "firebase/firestore";
+import { collection, onSnapshot, doc, deleteDoc } from "firebase/firestore";
+
 import "./UserStoredFiles.css";
 import binImage from "../../assets/bin.svg";
-
-// function createFileData(name, size, dateOfUpload, type, panelLink) {
-//   return { name, size, dateOfUpload, type, panelLink };
-// }
 
 export default function UserStoredFiles() {
   const [fetchedDocs, setFetchedDocs] = useState([
     {
-      fileName: "demo",
-      fileSize: "demo",
-      fileType: "demo",
-      savedAt: "demo",
-      panelLink: "demo",
+      id: "--",
+      fileName: "--",
+      fileSize: "--",
+      fileType: "--",
+      savedAt: "--",
+      panelLink: "--",
     },
   ]);
-
   const displayStatusClass = useStore(userSpecificAssetsDisplayStatus);
   const $newFileSaved = useStore(newFileSaved);
   const $currentUserUid = useStore(currentUserUid);
+  const $filesfetchedFromFirestore = useStore(filesfetchedFromFirestore);
 
   useEffect(() => {
     if ($currentUserUid) {
       const userCollectionRef = collection(db, $currentUserUid);
-      console.log("userCollectionRef : ", userCollectionRef);
-      const addFileDocToUserCollection = async (userCollectionRef) => {
-        try {
-          const docFetched = await getDocs(userCollectionRef);
-          console.log("Document fetched: ", docFetched.data);
-          setFetchedDocs(docFetched.data);
-        } catch (e) {
-          console.error("Error adding document: ", e);
-        }
-      };
-      addFileDocToUserCollection(userCollectionRef);
+      // console.log("userCollectionRef : ", userCollectionRef);
+      onSnapshot(userCollectionRef, (querySnapshot) => {
+        const tempSavedFilesDataArray = [];
+        querySnapshot.forEach((doc) => {
+          tempSavedFilesDataArray.push({ id: doc.id, ...doc.data() });
+        });
+        // console.log("tempSavedFilesDataArray : ", tempSavedFilesDataArray);
+
+        setFetchedDocs(tempSavedFilesDataArray);
+        filesfetchedFromFirestore.set(tempSavedFilesDataArray);
+      });
       newFileSaved.set(false);
     }
-  }, [$newFileSaved]);
+  }, [$newFileSaved, $currentUserUid]);
 
-  // const fileList = [
-  //   createFileData(
-  //     "file 1",
-  //     "28 KB",
-  //     "22-03-2023",
-  //     "application/vnd.ms-excel",
-  //     "---url here---"
-  //   ),
-  //   createFileData(
-  //     "file 1",
-  //     "28 KB",
-  //     "22-03-2023",
-  //     "application/vnd.ms-excel",
-  //     "---url here---"
-  //   ),
-  //   createFileData(
-  //     "file 1",
-  //     "28 KB",
-  //     "22-03-2023",
-  //     "application/vnd.ms-excel",
-  //     "---url here---"
-  //   ),
-  //   createFileData(
-  //     "file 1",
-  //     "28 KB",
-  //     "22-03-2023",
-  //     "application/vnd.ms-excel",
-  //     "---url here---"
-  //   ),
-  // ];
+  const depopulateSavedFilesContainer = (fileID) => {
+    const documentID = fileID;
+    if ($currentUserUid) {
+      const userDocRef = doc(db, $currentUserUid, documentID);
+      console.log("userDocRef : ", userDocRef);
+      const deleteDocWithUserDocRef = async (userDocRef) => {
+        try {
+          await deleteDoc(userDocRef);
+          console.log("Document deleted with ID: ", documentID);
+        } catch (error) {
+          console.error("Error deleting document: ", error);
+        }
+      };
+      deleteDocWithUserDocRef(userDocRef);
+    }
+  };
 
   return (
     <div className={displayStatusClass}>
@@ -98,15 +78,20 @@ export default function UserStoredFiles() {
             </tr>
           </thead>
           <tbody>
-            {fetchedDocs.map((file, index) => {
+            {$filesfetchedFromFirestore.map((file, index) => {
               return (
-                <tr id={index}>
+                <tr key={index}>
                   <td>{file.fileName}</td>
                   <td>{file.fileSize}</td>
                   <td>{file.savedAt}</td>
                   <td>{file.fileType}</td>
                   <td>{file.panelLink}</td>
-                  <td>
+                  <td
+                    id="deleteDoc"
+                    onClick={() => {
+                      depopulateSavedFilesContainer(file.id);
+                    }}
+                  >
                     <img id="binImage" src={binImage} width="16" alt="bin" />
                   </td>
                 </tr>
