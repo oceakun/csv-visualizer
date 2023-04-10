a805d1714e0ffd25e506641bf8db3e88e2157fbf
Sagar Deep <sagardeep1811@hotmail.com>
2023-04-11 01:08:53 +0530

    modify content to assimilate documentation better

--- src/components/instructionsContainer/InstructionsContainer.jsx
+++ src/components/instructionsContainer/InstructionsContainer.jsx
@@ -1,16 +1,35 @@
 import React from "react";
-
+import "./InstructionsContainer.css";
 export default function InstructionsContainer() {
   return (
     <div className="instructionsContainer">
-      <h1>Hi!</h1>
-      <ul>
-        <li>press 'Upload', a modal window will appear </li>
-        <li>
-          browse through your computer to choose a file, then press 'submit'
-        </li>
-        <li>press 'Visualize'</li>
-      </ul>
+      <h1>Brief summary of the documentation goes here</h1>
+      <div className="individualDiv">
+        <h2>Use as such</h2>
+        <p>
+          <mark>&#707;</mark> press 'Upload', a modal window will appear{" "}
+        </p>
+        <p>
+          <mark>&#707;</mark> browse through your computer, choose a file, press
+          'submit'
+        </p>
+        <p>
+          <mark>&#707;</mark> press 'Visuapze'
+        </p>
+      </div>
+      <div className="individualDiv">
+        <h2>Sign-up and use</h2>
+        <p>
+          <mark>&#707;</mark> press 'Upload', a modal window will appear{" "}
+        </p>
+        <p>
+          <mark>&#707;</mark> browse through your computer, choose a file, press
+          'submit'
+        </p>
+        <p>
+          <mark>&#707;</mark> press 'Visuapze'
+        </p>
+      </div>
     </div>
   );
 }
