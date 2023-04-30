fe4c233674c70a87af66be3f4681a1a1d1e51392
Sagar Deep <oceakun@pop-os.localdomain>
2023-04-30 23:55:41 +0530

    style to inhance readability and position centrally

--- frontend/src/components/instructionsContainer/InstructionsContainer.jsx
+++ frontend/src/components/instructionsContainer/InstructionsContainer.jsx
@@ -3,33 +3,55 @@ import "./InstructionsContainer.css";
 export default function InstructionsContainer() {
   return (
     <div className="instructionsContainer">
-      <h1>Brief summary of the documentation goes here</h1>
-      <div className="individualDiv">
-        <h2>Use as such</h2>
-        <p>
-          <mark>&#707;</mark> press 'Upload', a modal window will appear{" "}
-        </p>
-        <p>
-          <mark>&#707;</mark> browse through your computer, choose a file, press
-          'submit'
-        </p>
-        <p>
-          <mark>&#707;</mark> press 'Visuapze'
-        </p>
+      <div className="individualDivCover">
+        <h3>Use as such</h3>
+        <div className="individualDiv">
+          <p>
+            <mark>&#707;</mark> press 'Upload', a modal window will appear{" "}
+          </p>
+          <p>
+            <mark>&#707;</mark> browse through your computer, choose a file, press
+            'submit'
+          </p>
+          <p>
+            <mark>&#707;</mark> press 'Visualize'
+          </p>
+        </div>
       </div>
-      <div className="individualDiv">
-        <h2>Sign-up and use</h2>
-        <p>
-          <mark>&#707;</mark> press 'Upload', a modal window will appear{" "}
-        </p>
-        <p>
-          <mark>&#707;</mark> browse through your computer, choose a file, press
-          'submit'
-        </p>
-        <p>
-          <mark>&#707;</mark> press 'Visuapze'
-        </p>
+
+      <div className="individualDivCover">
+        <h3>Sign-up and use</h3>
+        <div className="individualDiv">
+          <p>
+            <mark>&#707;</mark> press 'Upload', a modal window will appear{" "}
+          </p>
+          <p>
+            <mark>&#707;</mark> browse through your computer, choose a file, press
+            'submit'
+          </p>
+          <p>
+            <mark>&#707;</mark> press 'Visualize'
+          </p>
+        </div>
       </div>
+
+      <div className="individualDivCover">
+        <h3>Sign-up and use</h3>
+        <div className="individualDiv">
+          <p>
+            <mark>&#707;</mark> press 'Upload', a modal window will appear{" "}
+          </p>
+          <p>
+            <mark>&#707;</mark> browse through your computer, choose a file, press
+            'submit'
+          </p>
+          <p>
+            <mark>&#707;</mark> press 'Visualize'
+          </p>
+        </div>
+      </div>
+
+      
     </div>
   );
 }
