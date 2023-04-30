43d9d3412f1f5c57fd589629a069c6993d5fb9b0
Sagar Deep <oceakun@pop-os.localdomain>
2023-05-01 00:00:51 +0530

    style to resize aptly and change color scheme

--- frontend/src/components/signIn/SignIn.jsx
+++ frontend/src/components/signIn/SignIn.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
-import { userSpecificAssetsDisplayStatus, saveButtonDisplayStatus} from "../../store/ComponentsDisplayPropertyStore";
+import { userSpecificAssetsDisplayStatus, saveButtonDisplayStatus } from "../../store/ComponentsDisplayPropertyStore";
 import { auth } from "../../firebase";
 import { currentUserUid, userEmail } from "../../store/UserStore";
 import "./SignIn.css";
@@ -59,49 +59,43 @@ function SignIn() {
     const val = e.target.value;
     setLoginPassword(val);
   };
-  //
+
   return (
-    <div className="SignInContainer">
-      <div className="LoginContainer">
-        <h3>Log-In</h3>
-        <div className="InputFieldContainer">
-          <div className={passwordAndEmailNotMatchWarningClass}>
-            <p style={{ textAlign: "left" }}>
-              * password and email <br />
-              don't match
-            </p>
-          </div>
-
-          <p>E-mail</p>
-          <input
-            type="email"
-            onChange={handleEmail}
-            value={loginEmail}
-            name="email"
-            required
-          ></input>
+    <div id="SignInContainer">
+      <h3>Log-In</h3>
+      <div className="SignInInputFieldContainer">
+        <div className={passwordAndEmailNotMatchWarningClass}>
+          <p style={{ textAlign: "left" }}>
+            * Password and email
+            don't match
+          </p>
         </div>
 
-        <div className="InputFieldContainer">
-          <p>Password</p>
-          <input
-            type="password"
-            onChange={handlePassword}
-            value={loginPassword}
-            name="password"
-            required
-          ></input>
-        </div>
+        <p>E-mail</p>
+        <input
+          type="email"
+          onChange={handleEmail}
+          value={loginEmail}
+          name="email"
+          required
+        ></input>
+      </div>
 
-        <div className="SubmitButtonContainer">
-          <button type="submit" onClick={logIn}>
-            Sign-In
-          </button>
-        </div>
+      <div className="SignInInputFieldContainer">
+        <p>Password</p>
+        <input
+          type="password"
+          onChange={handlePassword}
+          value={loginPassword}
+          name="password"
+          required
+        ></input>
+      </div>
 
-        <p id="alternativeChoice">
-          New here ?{" "}
-        </p>
+      <div id="SignInButtonContainer">
+        <button type="submit" onClick={logIn}>
+          Sign-In
+        </button>
       </div>
     </div>
   );
